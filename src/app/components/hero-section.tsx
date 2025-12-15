0f7131c51717746056399aab11a3c54cf2b07138
PiyushJ1 <piyxsh@gmail.com>
2025-12-16 01:12:28 +1100

    refactor: use alias imports

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -2,7 +2,7 @@
 import { Logo } from "./logo";
 import Link from "next/link";
 import { useState } from "react";
-import { Button } from "./ui/button";
+import { Button } from "@/app/components/ui/button";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-// import HeroSection from "@components/hero-section";
-import HeroSection from "./components/hero-section";
+import HeroSection from "@/app/components/hero-section";
 
 export default function Home() {
   return <HeroSection />;
