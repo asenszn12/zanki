3d14c45d3fc514eab27b3cb9db9d2b5e75b2a249
PiyushJ1 <piyxsh@gmail.com>
2025-12-16 00:27:16 +1100

    refactor: landing page files and directories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import HeroSection from "./landing/components/hero-section";
+import HeroSection from "./components/hero-section";
 
 export default function Home() {
-  return <HeroSection />; 
+  return <HeroSection />;
 }
