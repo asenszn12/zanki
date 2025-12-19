51c8d840c444682dd5b426f1ba593b86505bba61
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2025-12-19 20:28:37 +1100

    removed unusued landing directory

--- src/app/landing/.next/dev/static/chunks/src_app_landing_app_favicon_ico_mjs_59b8786b._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/a7e39_next_dist_25a76b1f._.js"
-],
-    source: "dynamic"
-});
