ef922ae45e3f8370530eef17aaeb68a0b7be4f8f
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2026-02-16 20:04:46 +1100

    making api call consistent

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -37,7 +37,7 @@ export async function GET() {
   `;
 
   try {
-  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.PPLX_API_KEY}`, {
+  const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.PPLX_API_KEY}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
